95614ca4edb2016a400c7a98901308b6dcbdb22e
krishna <kt827047@gmail.com>
2022-06-02 15:44:53 +0530

    using state and props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 // import { getByTitle } from '@testing-library/react';
 import Navbar from './Navbar';
+import Textform from './Textform';
 
 function App() {
   return (
     <>
-     <Navbar title ={"textFormate1"}/>
+     <Navbar /*title ={"textFormate1"} */ />
+     <div className="container">
+     <Textform />
+     </div>
      </>
     );
 }
